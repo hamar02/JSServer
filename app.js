20ff9c55eb9e519e6f7fd7a690c7b4fc3568cb2b
hamar02 <hampuz.aronsson@gmail.com>
2020-02-29 13:03:35 +0100

    can communicate with ue4

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const http = require("http");
-const requestHandler = require("./routes");
+const express = require("express");
+const bodyParser = require('body-parser')
 
-function rqListener(req, res){
-    requestHandler(req, res);
-};
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const server = http.createServer(rqListener);
+app.post('/UE4',function(req,res){
+    console.log("Getting UE4 Message");
+    var user_name=req.body.user;
+    var password=req.body.password;
 
-server.listen(3000);
+    console.log("UE4 Message: "+req.body);
+    console.log("UE4 Message: "+req.body.key);
+    console.log("UE4 Message: "+req.body.value);
 
-console.log("gg");
+});
+
+app.use("/gg",(req, res, next)=>{
+    console.log("this always runs");
+    next();
+});
+
+app.use("/gg",(req, res, next)=>{
+    console.log("mid");
+    res.send("<h1>gg</h1>");
+
+    //next();
+});
+
+
+
+app.use("/", (req, res, next)=>{
+    console.log("Index");
+    res.send("<h1>express</h1>");
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
